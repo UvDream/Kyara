2bd46d0a1ee4a9c44e083e541bf41f2560b1e251
wangzhongjie <uvdream@163.com>
2020-08-21 17:19:35 +0800

    fix🐛 修复表初始化

--- server/initialize/db_table.go
+++ server/initialize/db_table.go
@@ -8,21 +8,21 @@ import (
 // 注册数据库表专用
 func DBTables() {
 	db := global.GVA_DB
-	db.AutoMigrate(&model.SysUser{},
-		&model.SysAuthority{},
-		&model.SysApi{},
-		&model.SysBaseMenu{},
-		&model.JwtBlacklist{},
-		&model.SysWorkflow{},
-		&model.SysWorkflowStepInfo{},
-		&model.SysDictionary{},
-		&model.SysDictionaryDetail{},
-		&model.ExaFileUploadAndDownload{},
-		&model.ExaFile{},
-		&model.ExaFileChunk{},
-		&model.ExaCustomer{},
-		&model.SysOperationRecord{},
-		&model.SysArticle{},
+	db.AutoMigrate(model.SysUser{},
+		model.SysAuthority{},
+		model.SysApi{},
+		model.SysBaseMenu{},
+		model.JwtBlacklist{},
+		model.SysWorkflow{},
+		model.SysWorkflowStepInfo{},
+		model.SysDictionary{},
+		model.SysDictionaryDetail{},
+		model.ExaFileUploadAndDownload{},
+		model.ExaFile{},
+		model.ExaFileChunk{},
+		model.ExaCustomer{},
+		model.SysOperationRecord{},
+		model.SysArticle{},
 	)
 	global.GVA_LOG.Debug("注册成功!")
 }
