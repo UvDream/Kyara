2373f5b014fbd996fc182aae072bc9b182534351
wangzhongjie <uvdream@163.com>
2020-08-24 17:40:28 +0800

    fix🐛 优化用户表

--- server/model/sys_user.go
+++ server/model/sys_user.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 )
 
 type SysUser struct {
@@ -11,7 +11,7 @@ type SysUser struct {
 	Username    string       `json:"userName" gorm:"comment:'用户登录名'"`
 	Password    string       `json:"-"  gorm:"comment:'用户登录密码'"`
 	NickName    string       `json:"nickName" gorm:"default:'系统用户';comment:'用户昵称'" `
-	HeaderImg   string       `json:"headerImg" gorm:"default:'http://qmplusimg.henrongyi.top/head.png';comment:'用户头像'"`
+	HeaderImg   string       `json:"headerImg" gorm:"default:'https://gitee.com/Uvdream/images/raw/master/images/20200812100455.png';comment:'用户头像'"`
 	Authority   SysAuthority `json:"authority" gorm:"ForeignKey:AuthorityId;AssociationForeignKey:AuthorityId;comment:'用户角色'"`
 	AuthorityId string       `json:"authorityId" gorm:"default:888;comment:'用户角色ID'"`
 }
