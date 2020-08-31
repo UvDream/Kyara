3352e32567c3efb8d0db7c44cc1414b6d7d08a28
wangzhongjie <uvdream@163.com>
2020-08-31 15:56:06 +0800

    feat✨ 引入ui框架

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,18 @@ import { BlogComponent } from './blog/blog.component';
 import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './blog/home/home.component';
 import { DetailComponent } from './blog/detail/detail.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,8 +31,14 @@ import { DetailComponent } from './blog/detail/detail.component';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
