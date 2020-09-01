6b0322c6591c34bfb04c2f7df23726afcf302cb6
wangzhongjie <uvdream@163.com>
2020-09-01 17:29:50 +0800

    config🔧 配置文件

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { BlogComponent } from './blog/blog.component';
 import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './blog/home/home.component';
-import { DetailComponent } from './blog/detail/detail.component';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
@@ -21,14 +18,7 @@ import { BlogModule } from './blog/blog.module';
 
 registerLocaleData(zh);
 @NgModule({
-  declarations: [
-    AppComponent,
-    // BlogComponent,
-    AdminComponent,
-    // HomeComponent,
-    // DetailComponent,
-    // MarkdownComponent,
-  ],
+  declarations: [AppComponent, AdminComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
