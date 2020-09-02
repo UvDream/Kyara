dd41cf90ef015880a97506a8db19d3521681991f
wangzhongjie <uvdream@163.com>
2020-09-02 17:27:11 +0800

    fix🐛 简陋版本

--- my-app/src/app/blog/blog.module.ts
+++ my-app/src/app/blog/blog.module.ts
@@ -6,8 +6,9 @@ import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { ListComponent } from './home/list/list.component';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 @NgModule({
-  imports: [CommonModule, BlogRoutingModule, NzButtonModule],
+  imports: [CommonModule, BlogRoutingModule, NzButtonModule, NzGridModule],
   exports: [BlogComponent],
   declarations: [BlogComponent, HomeComponent, DetailComponent, ListComponent],
   providers: [],
