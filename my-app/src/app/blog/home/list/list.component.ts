c356a3459bc74e4ad97972de0a5de6f246d3dfc2
wangzhongjie <uvdream@163.com>
2020-09-03 09:29:29 +0800

    feat✨ 跳转详情

--- my-app/src/app/blog/home/list/list.component.ts
+++ my-app/src/app/blog/home/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,7 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./list.component.less'],
 })
 export class ListComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
   @Input() data: any;
   ngOnInit(): void {}
+  // tslint:disable-next-line:typedef
+  toDetail(data) {
+    this.router.navigate(['/detail/' + data.ID]);
+  }
 }
