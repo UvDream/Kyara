695eb25c1fa2af95e14ee3a150d0b06f799f2a21
wangzhongjie <uvdream@163.com>
2020-09-03 14:29:18 +0800

    fix🐛 跳转

--- my-app/src/app/blog/home/component/left-menus/navigation/navigation.component.ts
+++ my-app/src/app/blog/home/component/left-menus/navigation/navigation.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+interface MenuItem {
+  title: string;
+  id: number;
+  icon: string;
+  url: string;
+}
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.less'],
 })
 export class NavigationComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
   public menus = [
-    { title: '首页', id: 1, icon: 'uv-home-outline' },
-    { title: '仓库', id: 2, icon: 'uv-git-merge-outline' },
-    { title: '相册', id: 6, icon: 'uv-image-outline' },
-    { title: '归档', id: 3, icon: 'uv-file-tray-full-outline' },
-    { title: '留言', id: 4, icon: 'uv-at-sharp' },
-    { title: '关于', id: 5, icon: 'uv-cafe-outline' },
+    { title: '首页', id: 1, icon: 'uv-home-outline', url: '/home' },
+    { title: '仓库', id: 2, icon: 'uv-git-merge-outline', url: '/home' },
+    { title: '相册', id: 6, icon: 'uv-image-outline', url: '/home' },
+    { title: '归档', id: 3, icon: 'uv-file-tray-full-outline', url: '/home' },
+    { title: '留言', id: 4, icon: 'uv-at-sharp', url: '/home' },
+    { title: '关于', id: 5, icon: 'uv-cafe-outline', url: '/home' },
   ];
   ngOnInit(): void {}
+  // tslint:disable-next-line:typedef
+  navFunc(item: MenuItem) {
+    this.router.navigate([item.url]);
+  }
 }
