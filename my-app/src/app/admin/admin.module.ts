b09f4872950c4b7b4b7d24432366b93d079948fb
wangzhongjie <uvdream@163.com>
2020-09-07 14:34:46 +0800

    fix🐛 以及后台管理

---
+++ my-app/src/app/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { AdminRoutingModule } from './admin-routing.module'
+@NgModule({
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ],
+  declarations: [AdminComponent],
+  exports: [AdminComponent]
+})
+export class AdminModule { }
