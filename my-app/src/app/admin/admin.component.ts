791a12bf7617c3fd80612cb29b5cffbdf1ee65ae
wangzhongjie <uvdream@163.com>
2020-09-07 17:10:18 +0800

    fix🐛 修复sass

--- my-app/src/app/admin/admin.component.ts
+++ my-app/src/app/admin/admin.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.less']
 })
 export class AdminComponent implements OnInit {
 
