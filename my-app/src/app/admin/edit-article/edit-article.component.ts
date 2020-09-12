986b054e27cb4a06f37f9ebf05cb18f286c559d9
wangzhongjie <uvdream@163.com>
2020-09-12 21:09:11 +0800

    feat✨ 解决window

--- my-app/src/app/admin/edit-article/edit-article.component.ts
+++ my-app/src/app/admin/edit-article/edit-article.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
 @Component({
   selector: 'app-edit-article',
   templateUrl: './edit-article.component.html',
-  styleUrls: ['./edit-article.component.less']
+  styleUrls: ['./edit-article.component.less'],
 })
 export class EditArticleComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Client only code.
+      console.log(window);
+    }
+    if (isPlatformServer(this.platformId)) {
+      // Server only code.
+    }
   }
-
 }
