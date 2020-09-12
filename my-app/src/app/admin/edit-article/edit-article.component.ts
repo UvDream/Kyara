dff4bcdfbbeef402c79141c292176fdedddf98de
wangzhongjie <uvdream@163.com>
2020-09-12 21:17:23 +0800

    fix🐛 删除了

--- my-app/src/app/admin/edit-article/edit-article.component.ts
+++ my-app/src/app/admin/edit-article/edit-article.component.ts
@@ -1,20 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
-import { PLATFORM_ID } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-edit-article',
   templateUrl: './edit-article.component.html',
   styleUrls: ['./edit-article.component.less'],
 })
 export class EditArticleComponent implements OnInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      // Client only code.
-      console.log(window);
-    }
-    if (isPlatformServer(this.platformId)) {
-      // Server only code.
-    }
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
