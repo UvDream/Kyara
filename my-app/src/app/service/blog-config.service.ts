4fd3311ae74faf89327ff37aad888690870cb0f0
wangzhongjie <uvdream@163.com>
2020-11-04 11:48:58 +0800

    feat✨ 处理时间

--- my-app/src/app/service/blog-config.service.ts
+++ my-app/src/app/service/blog-config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ArticleService } from './article.service'
+import { ArticleService } from './article.service';
+import { toTimeDH } from '../../util/util';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,10 +13,10 @@ export class BlogConfigService {
   public BlogLogo = '';
   public AuthorAvatar = '';
   public ArticleCount = '';
-  public BlogStartTime = '';
+  public BlogTime = '';
+  public ActiveTime = '';
   getConfig = async () => {
     const res = await this.httpService.getConfig();
-    console.log(res, '获取配置');
     if (res.code !== 200) { return; }
     this.BlogName = res.data.blog_name;
     this.AuthorName = res.data.author_name;
@@ -23,6 +24,8 @@ export class BlogConfigService {
     this.BlogLogo = res.data.blog_logo;
     this.AuthorAvatar = res.data.author_avatar;
     this.ArticleCount = res.data.article_count;
-    this.BlogStartTime = res.data.blog_start_time;
+    // 处理时间
+    this.BlogTime = toTimeDH(res.data.blog_start_time, 'DD-HH');
+    this.ActiveTime = toTimeDH(res.data.active_time, 'DD');
   }
 }
--- my-app/src/util/util.ts
+++ my-app/src/util/util.ts
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2020-09-02 15:19:45
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2020-09-13 21:19:27
+ * @LastEditTime: 2020-11-04 11:48:22
  * @Description:工具函数
  * @Email: UvDream@163.com
  */
@@ -21,4 +21,19 @@ export function getBase64(file: File): Promise<string | ArrayBuffer | null> {
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
   });
+}
+export function toTimeDH(timeDate: string, type: string): string {
+  const time = new Date(timeDate).getTime();
+  const nowTime = new Date().getTime();
+  const diffTime = nowTime - time;
+  const day = diffTime / 1000 / 60 / 60 / 24;
+  const dayRound = Math.floor(day);
+  const hours = diffTime / 1000 / 60 / 60 - (24 * dayRound);
+  const hoursRound = Math.floor(hours);
+  if (type === 'DD') {
+    return dayRound + '天前';
+  } else if (type === 'DD-HH') {
+    return dayRound + '天' + hoursRound + '小时';
+  }
+
 }
