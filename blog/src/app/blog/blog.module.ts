d79b79731ba4dec4c718dde7814dff7194e08e49
wangzhongjie <uvdream@163.com>
2020-11-11 17:10:53 +0800

    fix🐛 删除一个没必要的文件夹

--- blog/src/app/blog/blog.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BlogComponent } from './blog.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [BlogComponent],
-})
-export class BlogModule {}
