af986de9288a0829e24a984ee603c2d1b113066e
wangzhongjie <uvdream@163.com>
2020-11-20 11:08:11 +0800

    feat✨ 测试一下获取ip地址

--- server/api/v1/sys_articles.go
+++ server/api/v1/sys_articles.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"fmt"
+	"net"
 	"server/global/response"
 	resp "server/model/response"
 	"server/service/article"
@@ -112,12 +113,30 @@ func GetGithub(c *gin.Context)  {
 //博客访问量统计
 func ViewBlogCount(c *gin.Context)  {
 	//id := c.Query("id")
+	fmt.Println(GetLocalIP(),"获取ip地址1")
+	fmt.Println(c.ClientIP(),"获取ip地址2")
 	err,msg:= article.ViewBlogCount(c)
 	if err != nil {
 		response.FailWithMessage(msg, c)
 	} else {
 		response.OkWithMessage(msg,c)
 	}
+}
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
 //获取公告
 func GetNotice(c *gin.Context)  {
