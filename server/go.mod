c0cdd6dfad76aaa0ef8bd67794e2ee6d2da11d34
wangzhongjie <uvdream@163.com>
2021-02-01 09:46:54 +0800

    feat✨ 统计文章字数

--- server/go.mod
+++ server/go.mod
@@ -60,4 +60,5 @@ require (
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/ini.v1 v1.55.0 // indirect
+	mvdan.cc/xurls/v2 v2.2.0
 )
--- server/service/admin/article.go
+++ server/service/admin/article.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"fmt"
+	"github.com/88250/lute"
 	"server/global"
 	"server/model"
 	"server/service/blog"
+	"server/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,7 +25,11 @@ func AddArticle(r model.SysArticle, c *gin.Context) (err error, msg string, data
 	if r.UserID == "" {
 		r.UserID = "ce0d6685-c15f-4126-a5b4-890bc9d2356d"
 	}
-
+	luteEngine := lute.New()
+	r.ArticleHtml = luteEngine.MarkdownStr("UvDream", r.ArticleContent)
+	counter := &utils.Counter{}
+	counter.Stat(string(r.ArticleHtml))
+	r.WordCount=counter.Total
 	if r.ArticleID == 0 {
 		//增加文章
 		err := db.Create(&r).Error
