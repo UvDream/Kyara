974bc6b2c8ac67120ebfff34484b84a55f4a4e19
wangzhongjie <uvdream@163.com>
2021-02-06 15:46:01 +0800

    fix🐛 解决一些问题

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
   { path: 'account', loadChildren: () => import('./account/account.module').then((m) => m.LoginModule) },
-  { path: '**', component: NotFoundComponent },
+  // { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
