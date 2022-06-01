ed0a0352181381ccb5dd06d855d6c86d5717207d
wangzhongjie <uvdream@163.com>
2022-06-01 16:26:21 +0800

    feat: 连接数据库

--- server/main.go
+++ server/main.go
@@ -8,17 +8,17 @@ import (
 )
 
 func main() {
-	//初始化配置文件
+	// 初始化配置文件
 	global.Viper = core.Viper()
-	//	初始化日志
+	// 初始化日志
 	global.Log = core.Zap()
 	zap.ReplaceGlobals(global.Log)
-	//初始化数据库
+	// 初始化数据库
 	global.DB = initialize.Gorm()
 	if global.DB != nil {
-		//初始化表
+		// 初始化表
 		initialize.RegistrationTable(global.DB)
-		//	程序结束关闭数据库链接
+		// 程序结束关闭数据库链接
 		db, _ := global.DB.DB()
 		defer db.Close()
 	}
