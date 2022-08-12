edd2a8763888edbe4a332f7a2fbc4bd5658a345e
wangzhongjie <uvdream@163.com>
2022-08-12 16:40:32 +0800

    fix: 启动redis

--- server/scripts/dependencies.sh
+++ server/scripts/dependencies.sh
@@ -2,3 +2,6 @@
 
 sudo docker rm -f kyara-mysql
 sudo docker run -d --name kyara-mysql  -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=kyara -p 3306:3306  mysql:oracle
+
+sudo docker rm -f kyara-redis
+sudo docker run -d --name kyara-redis -p 6379:6379 redis:latest --requirepass "redispw"
